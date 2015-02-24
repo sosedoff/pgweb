3f21160545e6b933e69db4cf96ab3a8b4c77c21a
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-02-23 20:17:03 -0600

    Version bump: 0.5.1

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const VERSION = "0.5.0"
+const VERSION = "0.5.1"
 
 type Options struct {
 	Version  bool   `short:"v" long:"version" description:"Print version"`
