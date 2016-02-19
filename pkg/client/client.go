4b4f7783608c7f506aad37cb11ef2d55854b181d
Dan Sosedoff <dan.sosedoff@gmail.com>
2016-02-18 22:36:01 -0600

    Return number of affected rows if sql query is update or delete

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -226,8 +226,29 @@ func (client *Client) Query(query string) (*Result, error) {
 }
 
 func (client *Client) query(query string, args ...interface{}) (*Result, error) {
-	rows, err := client.db.Queryx(query, args...)
+	action := strings.ToLower(strings.Split(query, " ")[0])
+	if action == "update" || action == "delete" {
+		res, err := client.db.Exec(query, args...)
+		if err != nil {
+			return nil, err
+		}
 
+		affected, err := res.RowsAffected()
+		if err != nil {
+			return nil, err
+		}
+
+		result := Result{
+			Columns: []string{"Rows Affected"},
+			Rows: []Row{
+				Row{affected},
+			},
+		}
+
+		return &result, nil
+	}
+
+	rows, err := client.db.Queryx(query, args...)
 	if err != nil {
 		return nil, err
 	}
