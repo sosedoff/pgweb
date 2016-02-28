df135f8f27ff033ef8735480ba159b7a4b4dec81
goldfix <p.pietro@gmail.com>
2016-02-28 11:06:01 +0100

    modify parameter name and description

--- pkg/command/options.go
+++ pkg/command/options.go
@@ -24,7 +24,7 @@ type Options struct {
 	SkipOpen bool   `short:"s" long:"skip-open" description:"Skip browser open on start"`
 	Sessions bool   `long:"sessions" description:"Enable multiple database sessions" default:"false"`
 	Prefix   string `long:"prefix" description:"Add a url prefix"`
-	Timeout  int    `long:"timeout" description:"Query Timeout (Seconds)" default:"30"`
+	Timeout  int    `long:"query-timeout" description:"Abort statement that takes more number of seconds" default:"30"`
 }
 
 var Opts Options
