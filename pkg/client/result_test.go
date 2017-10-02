c2379c8fc46f74f35fb653d377850d3abcfec724
icalF <laser.survivor@gmail.com>
2017-10-02 17:02:14 +0700

    revert

--- pkg/client/result_test.go
+++ pkg/client/result_test.go
@@ -1,10 +1,10 @@
 package client
 
 import (
-	_"encoding/json"
+	"encoding/json"
 	"testing"
 
-	_"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_PrepareBigints(t *testing.T) {
@@ -21,11 +21,11 @@ func Test_PrepareBigints(t *testing.T) {
 
 	result.PrepareBigints()
 
-	// assert.Equal(t, 1234, result.Rows[0][0])
-	// assert.Equal(t, "9223372036854775807", result.Rows[1][0])
-	// assert.Equal(t, "-9223372036854775808", result.Rows[2][0])
-	// assert.Equal(t, "9.223372036854776e+18", result.Rows[3][0])
-	// assert.Equal(t, "9.999999999999999e+14", result.Rows[4][0])
+	assert.Equal(t, 1234, result.Rows[0][0])
+	assert.Equal(t, "9223372036854775807", result.Rows[1][0])
+	assert.Equal(t, "-9223372036854775808", result.Rows[2][0])
+	assert.Equal(t, "9.223372036854776e+18", result.Rows[3][0])
+	assert.Equal(t, "9.999999999999999e+14", result.Rows[4][0])
 }
 
 func Test_CSV(t *testing.T) {
@@ -37,17 +37,14 @@ func Test_CSV(t *testing.T) {
 		},
 	}
 
-	// expected
-	_ = "id,name,email\n1,John,john@example.com\n2,Bob,bob@example.com\n"
-	// output
-	_ = string(result.CSV())
+	expected := "id,name,email\n1,John,john@example.com\n2,Bob,bob@example.com\n"
+	output := string(result.CSV())
 
-	// assert.Equal(t, expected, output)
+	assert.Equal(t, expected, output)
 }
 
 func Test_JSON(t *testing.T) {
-	// result
-	_ = Result{
+	result := Result{
 		Columns: []string{"id", "name", "email"},
 		Rows: []Row{
 			Row{1, "John", "john@example.com"},
@@ -55,15 +52,13 @@ func Test_JSON(t *testing.T) {
 		},
 	}
 
-	// obj
-	_ = []struct {
+	obj := []struct {
 		Id    int
 		Name  string
 		Email string
 	}{}
 
-	// expected
-	_ = []struct {
+	expected := []struct {
 		Id    int
 		Name  string
 		Email string
@@ -72,7 +67,7 @@ func Test_JSON(t *testing.T) {
 		{2, "Bob", "bob@example.com"},
 	}
 
-	// assert.NoError(t, json.Unmarshal(result.JSON(), &obj))
-	// assert.Equal(t, 2, len(obj))
-	// assert.Equal(t, expected, obj)
+	assert.NoError(t, json.Unmarshal(result.JSON(), &obj))
+	assert.Equal(t, 2, len(obj))
+	assert.Equal(t, expected, obj)
 }
