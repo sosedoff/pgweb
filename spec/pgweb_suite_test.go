1f1dd9160f7e4929af4731725fe4c975ec5a5978
Vardan Pogosian <vardan.pogosyan@gmail.com>
2017-11-12 10:08:02 +0300

    Revert "fix(spec): update Godeps to include ginkgo dependencies"
    This reverts commit 58ace1f

--- spec/pgweb_suite_test.go
+++ spec/pgweb_suite_test.go
@@ -124,6 +124,25 @@ var _ = BeforeSuite(func() {
 	setup()
 
 	agoutiDriver = agouti.ChromeDriver()
+
+	// https://github.com/onsi/ginkgo/issues/285#issuecomment-290575636
+
+	// The Ginkgo test runner takes over os.Args and fills it with
+	// its own flags.  This makes the cobra command arg parsing
+	// fail because of unexpected options.  Work around this.
+
+	// Save a copy of os.Args
+	//origArgs := os.Args[:]
+
+	// Trim os.Args to only the first arg
+	//os.Args = os.Args[:1] // trim to only the first arg, which is the command itself
+
+	// Run the command which parses os.Args
+	//pwcli.Run()
+
+	// Restore os.Args
+	//os.Args = origArgs[:]
+
 	Expect(agoutiDriver.Start()).To(Succeed())
 })
 
