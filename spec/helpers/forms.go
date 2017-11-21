ffaad2ebf48dbaa6afd5d399d2cf76f3818fef3e
Vardan Pogosian <vardan.pogosyan@gmail.com>
2017-11-21 16:46:54 +0300

    feat(spec): add skeleton for main GUI tests

--- spec/helpers/forms.go
+++ spec/helpers/forms.go
@@ -6,7 +6,6 @@ import (
 	"github.com/sclevine/agouti"
 )
 
-
 const (
 	PgConnUrlSelector = "#connection_url"
 
@@ -19,8 +18,23 @@ const (
 )
 
 const (
-	CurrentDbSelector = "#current_database"
+	CurrentDbSelector       = "#current_database"
 	ConnectionErrorSelector = "#connection_error"
+)
+
+const (
+	TableBookSelector = ".schema-table[data-id='public.books']"
+)
+
+const (
+	TabRowsSelector       = "#table_content"
+	TabStructureSelector  = "#table_stucture"
+	TabIndexesSelector    = "#table_indexes"
+	TabConstaintsSelector = "#table_constraints"
+	TabQuerySelector      = "#table_query"
+	TabHistorySelector    = "#table_history"
+	TabActivitySelector   = "#table_activity"
+	TabConnectionSelector = "#table_connection"
 )
 
 func FillConnectionForm(page *agouti.Page, data map[string]string) {
@@ -29,9 +43,22 @@ func FillConnectionForm(page *agouti.Page, data map[string]string) {
 	}
 }
 
+func ConnectByStandardTab(page *agouti.Page) {
+	initVars()
 
+	data := map[string]string{
+		PgHostSelector: ServerHost,
+		PgPortSelector: ServerPort,
+		PgUserSelector: ServerUser,
+		PgPassSelector: ServerPassword,
+		PgDbSelector:   ServerDatabase,
+		PgSslSelector:  "disable",
+	}
+
+	FillConnectionForm(page, data)
+	page.FindByButton("Connect").Click()
+}
 
 func Screenshot(page *agouti.Page, name string) {
 	page.Screenshot(fmt.Sprintf("_output/%s.png", name))
 }
-
