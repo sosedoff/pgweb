8977a1662b2cc0393165056953c18fae53481eee
Vardan Pogosian <vardan.pogosyan@gmail.com>
2017-11-29 21:07:40 +0300

    fix(spec): wait connection to DB will be made and gui rendered

--- spec/gui_test.go
+++ spec/gui_test.go
@@ -1,30 +1,29 @@
 package spec
 
-//
-//import (
-//	. "github.com/onsi/ginkgo"
-//	. "github.com/onsi/gomega"
-//	. "github.com/sclevine/agouti/matchers"
-//
-//	"github.com/sosedoff/pgweb/spec/helpers"
-//)
-//
-//var _ = Describe("Gui", func() {
-//
-//	BeforeEach(func() {
-//		helpers.ConnectByStandardTab(page)
-//	})
-//
-//	Context("Tabs", func() {
-//		//BeforeEach(func() {
-//		//	sel := page.Find(helpers.TableBookSelector)
-//		//	sel.Click()
-//		//})
-//		//
-//		//It("clicks on Rows tab", func() {
-//		//	page.Find(helpers.TabRowsSelector).Click()
-//		//	Expect(page.Find("#results")).Should(BeVisible())
-//		//	Expect(page.Find("#results > tr td:nth-child(2)").Text()).To(Equal("The Shining"))
-//		//})
-//	})
-//})
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/sclevine/agouti/matchers"
+
+	"github.com/sosedoff/pgweb/spec/helpers"
+)
+
+var _ = Describe("Gui", func() {
+
+	BeforeEach(func() {
+		helpers.ConnectByStandardTab(page)
+		Eventually(page.Find(helpers.CurrentDbSelector), 10).Should(BeVisible())
+	})
+
+	FContext("Tabs", func() {
+		BeforeEach(func() {
+			page.Find(helpers.TableBookSelector).Click()
+		})
+
+		It("clicks on Rows tab", func() {
+			page.Find(helpers.TabRowsSelector).Click()
+			Expect(page.Find("#results")).Should(BeVisible())
+			Expect(page.All("#results tr td").At(1).Text()).To(Equal("The Shining"))
+		})
+	})
+})
