8c83a8da192c38b3ac00ef1014ee842f40eb1648
Vardan Pogosian <vardan.pogosyan@gmail.com>
2018-01-24 08:51:23 +0300

    fix(app.js): show #query_progress by display: inline

--- static/js/app.js
+++ static/js/app.js
@@ -624,7 +624,7 @@ function runExplain() {
   setCurrentTab("table_query");
 
   $("#run, #explain, #csv, #json, #xml").prop("disabled", true);
-  $("#query_progress").show();
+  $("#query_progress").css('display', 'inline');
 
   var query = $.trim(editor.getSelectedText() || editor.getValue());
 
