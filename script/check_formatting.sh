a02c813784a784c1bd14f0379f8e275e11d643ba
Dan Sosedoff <dan.sosedoff@gmail.com>
2018-03-16 19:59:58 -0500

    Check files with go fmt in CI

---
+++ script/check_formatting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+files="$(go fmt ./...)"
+ignore="pkg/data/bindata.go"
+files=${files[@]/$ignore}
+
+if [ -n "$files" ]; then
+  echo "Go code is not formatted: $files"
+  for file in $files; do
+    echo "----> $file"
+  done
+  exit 1
+fi
