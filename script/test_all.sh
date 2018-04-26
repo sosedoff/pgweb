ac14369e75679e7252f3cd264ceeb6b27d046b22
Dan Sosedoff <dan.sosedoff@gmail.com>
2018-04-26 00:39:35 -0500

    Test against all pg 10.x versions

--- script/test_all.sh
+++ script/test_all.sh
@@ -14,20 +14,16 @@ export PGPASSWORD=""
 export PGDATABASE="booktown"
 export PGPORT="15432"
 
-# TODO: Enable the 10.x branch when it's supported on Travis.
-# Local 10.x version is required so that pg_dump can properly work with older versions.
-# 10.x branch is normally supported.
-versions="9.1 9.2 9.3 9.4 9.5 9.6"
+versions="9.1 9.2 9.3 9.4 9.5 9.6 10.0 10.1 10.2 10.3"
 
 for i in $versions
 do
   export PGVERSION="$i"
-
-  echo "------------------------------- BEGIN TEST -------------------------------"
-  echo "Running tests against PostgreSQL v$PGVERSION"
-  docker rm -f postgres || true
+  echo "----------------------- PostgreSQL v$PGVERSION ------------------------"
+  echo "Removing existing container"
+  docker rm -f postgres || true 
+  echo "Starting contaienr for $PGVERSION"
   docker run -p $PGPORT:5432 --name postgres -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres:$PGVERSION
   sleep 5
   make test
-  echo "-------------------------------- END TEST --------------------------------"
 done
