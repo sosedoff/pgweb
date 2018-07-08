55bbec46a672e4bb50b2963edbd0a588e186f616
Vardan Pogosian <vardan.pogosyan@gmail.com>
2018-07-08 20:31:53 +0300

    feat: add install_chromedriver.sh script

---
+++ script/install_chromedriver.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Refactored script from https://gist.github.com/ziadoz/3e8ab7e944d02fe872c3454d17af31a5
+
+# https://developers.supportbee.com/blog/setting-up-cucumber-to-run-with-Chrome-on-Linux/
+# https://gist.github.com/curtismcmullan/7be1a8c1c841a9d8db2c
+# http://stackoverflow.com/questions/10792403/how-do-i-get-chrome-working-with-selenium-using-php-webdriver
+# http://stackoverflow.com/questions/26133486/how-to-specify-binary-path-for-remote-chromedriver-in-codeception
+# http://stackoverflow.com/questions/40262682/how-to-run-selenium-3-x-with-chrome-driver-through-terminal
+# http://askubuntu.com/questions/760085/how-do-you-install-google-chrome-on-ubuntu-16-04
+
+set -e
+
+# Versions
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+
+# Remove existing downloads and binaries so we can start from scratch.
+sudo apt-get remove google-chrome-stable
+rm ~/chromedriver_linux64.zip
+sudo rm /usr/local/bin/chromedriver
+
+# Install dependencies.
+sudo apt-get update
+sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
+
+# Install Chrome.
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+
+# Install ChromeDriver.
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
