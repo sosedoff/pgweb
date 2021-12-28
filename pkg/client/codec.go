3fe23487d128697d636176ccf32723361c085094
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-12-28 15:52:39 -0600

    Add missing file

---
+++ pkg/client/codec.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+const (
+	CodecNone   = "none"
+	CodecHex    = "hex"
+	CodecBase64 = "base64"
+)
+
+var (
+	// BinaryEncodingFormat specifies the default serialization format of binary data
+	BinaryCodec = CodecBase64
+)
+
+func SetBinaryCodec(codec string) error {
+	switch codec {
+	case CodecNone, CodecHex, CodecBase64:
+		BinaryCodec = codec
+	default:
+		return fmt.Errorf("invalid binary codec value: %v", codec)
+	}
+
+	return nil
+}
+
+func encodeBinaryData(data []byte) string {
+	switch BinaryCodec {
+	case CodecHex:
+		return hex.EncodeToString(data)
+	case CodecBase64:
+		return base64.StdEncoding.EncodeToString(data)
+	default:
+		return string(data)
+	}
+}
