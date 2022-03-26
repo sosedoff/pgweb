4da3ebd300abd0b713b9afd3a34a8e4c456a9dd5
Dan Sosedoff <dan.sosedoff@gmail.com>
2022-03-26 10:27:59 -0500

    Change binary codec back to none

--- pkg/command/options.go
+++ pkg/command/options.go
@@ -42,7 +42,7 @@ type Options struct {
 	ConnectionIdleTimeout        int    `long:"idle-timeout" description:"Set connection idle timeout in minutes" default:"180"`
 	Cors                         bool   `long:"cors" description:"Enable Cross-Origin Resource Sharing (CORS)"`
 	CorsOrigin                   string `long:"cors-origin" description:"Allowed CORS origins" default:"*"`
-	BinaryCodec                  string `long:"binary-codec" description:"Codec for binary data serialization, one of 'none', 'hex', 'base58', 'base64'" default:"base64"`
+	BinaryCodec                  string `long:"binary-codec" description:"Codec for binary data serialization, one of 'none', 'hex', 'base58', 'base64'" default:"none"`
 }
 
 var Opts Options
