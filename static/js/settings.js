52bf40117e20b4793aa7fbaf61ed0a6000555766
Nepta_ <lucas.besson@outlook.fr>
2025-11-21 21:28:02 +0100

    cleaning

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,53 +1,29 @@
-// This file manages the application settings, including loading and saving settings such as navbar color, logo, and theme preferences.
-
-// const settingsFilePath = path.join(__dirname, '../../settings.json');
-
 let appSettings = {
     navbarColor: '#79589f',
     logo: false,
     darkTheme: false
 };
 
-// function loadSettings() {
-//     if (fs.existsSync(settingsFilePath)) {
-//         const data = fs.readFileSync(settingsFilePath);
-//         appSettings = JSON.parse(data);
-//     }
-// }
-// function loadSettings() {
-//     $.getJSON('settings.json', function(data) {
-//         appSettings = data;
-//         applySettings();
-//     });
-// }
-
-// function saveSettings() {
-//     fs.writeFileSync(settingsFilePath, JSON.stringify(appSettings, null, 2));
-// }
 function saveSettings() {
     $.ajax({
         url: 'settings.json',
         method: 'PUT',
         contentType: 'application/json',
         data: JSON.stringify(appSettings),
-        success: function() {
+        success: function () {
             console.log('Settings saved successfully.');
         },
-        error: function() {
+        error: function () {
             console.error('Error saving settings.');
         }
     });
 }
 
 function applySettings(settings) {
-    // document.body.style.backgroundColor = settings.darkTheme ? '#333' : '#fff';
-    // document.querySelector('.navbar').style.backgroundColor = appSettings.navbarColor;
     document.documentElement.style.setProperty('--navbar-color', settings.navbarColor);
     if (settings.logo) {
         // Display logo wrapper
         $('#sidebar .sidebar-brand').css('display', 'block');
-        // Set image link
-        // $('#sidebar .sidebar-brand > img').attr('src', settings.logo);
     }
 }
 
@@ -65,18 +41,3 @@ function toggleTheme() {
     appSettings.darkTheme = !appSettings.darkTheme;
     saveSettings();
 }
-
-// function getSettings() {
-//     return appSettings;
-// }
-
-// Load settings on startup
-// loadSettings();
-
-// module.exports = {
-//     setNavbarColor,
-//     setLogo,
-//     toggleTheme,
-//     getSettings,
-//     loadSettings
-// };
