5389359eeaad9b3bbd3e2e6c3bf05d7bb39bb1d7
Sara Elzayat <st.elzayat@gmail.com>
2026-03-09 11:49:37 +0200

    Add timeouts to metrics HTTP server

--- pkg/metrics/server.go
+++ pkg/metrics/server.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -9,6 +10,17 @@ import (
 func StartServer(logger *logrus.Logger, path string, addr string) error {
 	logger.WithField("addr", addr).WithField("path", path).Info("starting prometheus metrics server")
 
-	http.Handle(path, NewHandler())
-	return http.ListenAndServe(addr, nil)
+	mux := http.NewServeMux()
+	mux.Handle(path, NewHandler())
+
+	server := &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 10 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	return server.ListenAndServe()
 }
